8d33483b2c5b7824852180858363f0f207fe20d9
Technoboy10 <technoboy10@fake.email>
2015-02-18 18:15:29 -0700

    index.html

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
 var http = require('http')
 var request = require('request')
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
 var port = process.env.PORT || 8080
 
 http.createServer(function (req, res) {
+  console.log(req.url);
   res.setTimeout(25000)
   res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.url == "/"){
+  	res.writeHead(200);
+  	res.write(index);
+  	res.end();
+  }else{
   try {
     request(req.url.slice(1), {encoding: null}, function(error, response, body) {
       res.write(body)
       res.end()
     })
   }
-  catch(e) {}
+  catch(e) {}}
 }).listen(port)
 
 console.log("Listening on port: " + port)
