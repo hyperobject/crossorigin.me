6efe9b05d36b312416ad42b0285971717cbad071
Technoboy10 <technoboy10@fake.email>
2015-02-20 09:35:32 -0700

    aaaah

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node backup.js
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var index = fs.readFileSync('index.html');
 var port = process.env.PORT || 8080
 
 var server = http.createServer(function(res, req){
+	console.log()
     if fs.existsSync(req.url.slice(1)){
     	res.writeHead(200);
     	res.write(fs.readFileSync(req.url.slice(1)));
