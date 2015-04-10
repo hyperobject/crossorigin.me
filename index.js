3b907315810ddb9b9e22964eda7037bac831e34b
Technoboy10 <technoboy10@fake.email>
2015-04-10 09:19:21 -0600

    fixes #2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var request = require('request')
 var fs = require('fs');
 var domain = require('domain');
 var index = fs.readFileSync('index.html');
+var favicon = fs.readFileSync('favicon.ico');
 
 var port = process.env.PORT || 8080
 
@@ -28,25 +29,31 @@ var server = http.createServer(function (req, res) {
 
 	function handler(req, res) {
 	console.log(req.url);
-	
-    if (fs.existsSync(req.url.slice(1))){
-    	res.writeHead(200);
-    	res.write(fs.readFileSync(req.url.slice(1)));
-    	res.end()
-    } else if (req.url == '/') {
-    	res.writeHead(200);
-    	res.write(index);
-    	res.end();
-    } else {
-    	try {
+	switch (req.url) {
+		case "/":
+			res.writeHead(200);
+			res.write(index);
+			res.end();
+			break;
+		case "/index.html":
+			res.writeHead(200);
+			res.write(index);
+			res.end();
+			break;
+		case "/favicon.ico":
+			res.writeHead(200);
+			res.write(favicon);
+			res.end()
+		default:
+			try {
 			res.setTimeout(25000);
 			res.setHeader('Access-Control-Allow-Origin', '*');
 			request(req.url.slice(1), {encoding: null}, function(error, response, body) {
       			res.write(body)
       			res.end()
     		})
-    	} catch (e) {
+    		} catch (e) {
     	   	res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
-    	}
-    }
+    		}
+	}
 }
