5eb8ef154c3a6260c4ecb92236f9bb318d2a9d80
Technoboy10 <technoboy10@fake.email>
2015-06-02 07:39:52 -0600

    Closes PR #5

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ var server = http.createServer(function (req, res) {
 			res.setTimeout(25000);
 			res.setHeader('Access-Control-Allow-Origin', '*');
 			res.setHeader('Access-Control-Allow-Credentials', false);
+			res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 			request(req.url.slice(1), {encoding: null}, function(error, response, body) {
       			res.write(body)
       			res.end()
