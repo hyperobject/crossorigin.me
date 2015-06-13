c88a20190d9edcec824685c80b2ee209eb93fc43
Bernie Sumption <bernie@berniecode.com>
2015-06-13 12:56:40 +0100

    Made tabs, spaces and semicolons consistent

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-var http = require('http')
-var request = require('request')
+var http = require('http');
+var request = require('request');
 var fs = require('fs');
 var domain = require('domain');
 var index = fs.readFileSync('index.html');
 var favicon = fs.readFileSync('favicon.ico');
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 
 var server = http.createServer(function (req, res) {
 	var d = domain.create();
@@ -14,7 +14,7 @@ var server = http.createServer(function (req, res) {
 		console.log('ERROR', e.stack);
 
 		res.statusCode = 500;
-		res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
+		res.end('Error: ' + ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
 	});
 
 	d.add(req);
@@ -27,7 +27,7 @@ var server = http.createServer(function (req, res) {
 }).listen(port);
 
 
-	function handler(req, res) {
+function handler(req, res) {
 	console.log(req.url);
 	switch (req.url) {
 		case "/":
@@ -43,23 +43,23 @@ var server = http.createServer(function (req, res) {
 		case "/favicon.ico":
 			res.writeHead(200);
 			res.write(favicon);
-			res.end()
+			res.end();
 		default:
 			if (req.url.indexOf('vivastreet') > -1){
 				res.end('tempbanned');
 			} else {
 			try {
-			res.setTimeout(25000);
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Credentials', false);
-			res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-			request(req.url.slice(1), {encoding: null}, function(error, response, body) {
-      			res.write(body)
-      			res.end()
-    		})
-    		} catch (e) {
-    	   	res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
-    		}
+				res.setTimeout(25000);
+				res.setHeader('Access-Control-Allow-Origin', '*');
+				res.setHeader('Access-Control-Allow-Credentials', false);
+				res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+				request(req.url.slice(1), {encoding: null}, function(error, response, body) {
+					res.write(body);
+					res.end();
+				});
+			} catch (e) {
+				res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
 			}
+		}
 	}
 }
