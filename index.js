143d2ebd4380c56ac90d3a061bcf407e3127d63b
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2015-12-01 14:16:42 +1000

    - Fixed missing break in index switch case (casing switch leakage, and header duplication attempts)  - Added the PNG icon, Chrome and Firefox seemed to be calling it, which was throwing errors to the log  - Brought the request options out into the global scope (save node recreating it every time `handler` is called)
    Signed-off-by: Shane Gadsby <schme16@gmail.com>

--- index.js
+++ index.js
@@ -5,68 +5,80 @@ var http = require('http'),
 	domain = require('domain'),
 	index = require('zlib').gzipSync(fs.readFileSync('index.html'))
 	favicon = require('zlib').gzipSync(fs.readFileSync('favicon.ico'))
+	faviconPNG = require('zlib').gzipSync(fs.readFileSync('favicon.png'))
 	port = process.env.PORT || 8080,
 	allowedOriginalHeaders = new RegExp('^' + require('./allowedOriginalHeaders.json').join('|'), 'i')
-	bannedUrls = new RegExp(require('./bannedUrls.json').join('|'), 'i')
+	bannedUrls = new RegExp(require('./bannedUrls.json').join('|'), 'i'),
+	requestOptions = {
+		encoding: null,
+		rejectUnauthorized: false,
+		headers: {
+			'accept-encoding': 'identity'
+		}
+	},
+	server = http.createServer(function (req, res) {
+		var d = domain.create();
+		d.on('error', function (e){
+			console.log('ERROR', e.stack);
 
-var server = http.createServer(function (req, res) {
-	var d = domain.create();
-	d.on('error', function (e){
-		console.log('ERROR', e.stack);
+			res.statusCode = 500;
+			res.end('Error: ' + ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
+		});
 
-		res.statusCode = 500;
-		res.end('Error: ' + ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
-	});
+		d.add(req);
+		d.add(res);
 
-	d.add(req);
-	d.add(res);
-
-	d.run(function() {
-		handler(req, res);
-	});
-}).listen(port);
-
-
-function handler(req, res) {
-	switch (req.url) {
-		case "/":
-		case "/index.html" :
-			res.setHeader('content-encoding', 'gzip')
-			res.setHeader('content-type', 'text/html')
-			res.writeHead(200);
-			res.write(index);
-			res.end();
-		case "/favicon.ico":
-			res.setHeader('content-encoding', 'gzip')
-			res.setHeader('content-type', 'image/x-icon')
-			res.writeHead(200);
-			res.write(favicon);
-			res.end();
-		case "/favicon.ico":
-			break;
-		default:
-			if (bannedUrls.test(req.url)) {
-				res.writeHead(403);
-				res.end('FORBIDDEN');
-			} else {
-			try {
-				res.setTimeout(25000);
-				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader('Access-Control-Allow-Credentials', false);
-				res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-				res.setHeader('Expires', new Date(Date.now() + 86400000).toUTCString()); // one day in the future
-				var r = request(req.url.slice(1), {encoding: null, rejectUnauthorized: false});
-				r.pipefilter = function(response, dest) {
-					for (var header in response.headers) {
-						if (!allowedOriginalHeaders.test(header)) {
-							dest.removeHeader(header);	
+		d.run(function() {
+			handler(req, res);
+		});
+	}).listen(port),
+	handler = function handler(req, res) {
+		switch (req.url) {
+			case "/":
+			case "/index.html" :
+				res.setHeader('content-type', 'text/html')
+				res.setHeader('content-encoding', 'gzip')
+				res.writeHead(200);
+				res.write(index);
+				res.end();
+				break;
+			case "/favicon.ico":
+				res.setHeader('content-encoding', 'gzip')
+				res.setHeader('content-type', 'image/x-icon')
+				res.writeHead(200);
+				res.write(favicon);
+				res.end();
+				break;
+			case "/favicon.png":
+				res.setHeader('content-encoding', 'gzip')
+				res.setHeader('content-type', 'image/png')
+				res.writeHead(200);
+				res.write(faviconPNG);
+				res.end();
+				break;
+			default:
+				if (bannedUrls.test(req.url)) {
+					res.writeHead(403);
+					res.end('FORBIDDEN');
+				} else {
+				try {
+					res.setTimeout(25000);
+					res.setHeader('Access-Control-Allow-Origin', '*');
+					res.setHeader('Access-Control-Allow-Credentials', false);
+					res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+					res.setHeader('Expires', new Date(Date.now() + 86400000).toUTCString()); // one day in the future
+					var r = request(req.url.slice(1), requestOptions);
+					r.pipefilter = function(response, dest) {
+						for (var header in response.headers) {
+							if (!allowedOriginalHeaders.test(header)) {
+								dest.removeHeader(header);	
+							}
 						}
-					}
-				};
-				r.pipe(res);
-			} catch (e) {
-				res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
+					};
+					r.pipe(res);
+				} catch (e) {
+					res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
+				}
 			}
 		}
 	}
-}
