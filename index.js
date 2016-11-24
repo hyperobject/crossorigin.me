17b3275c2d15dbec1a5fb7f26efe16198b658a55
Technoboy10 <technoboy10@fake.email>
2016-11-23 18:57:17 -0700

    remove memwatch stuff

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const server = require('./modules/server');
-var memwatch = require('memwatch-next');
-memwatch.on('leak', function(info) {console.log(info);}); //eslint-disable-line
 server.listen(process.env.PORT || 8080, function () {
   console.log('%s listening at %s', server.name, server.url); //eslint-disable-line
 });
