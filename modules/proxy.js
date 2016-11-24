96a12de5209a089ddfb6b092b00b58b0984aa7cc
tjvr <tim@tjvr.org>
2016-11-24 16:08:11 +0000

    Include client headers in request
    We were already including server headers (by mistake because of pipe() I
    think?), so let's not include all of those.

--- modules/proxy.js
+++ modules/proxy.js
@@ -1,16 +1,48 @@
 const request = require('request');
+
+let clientHeadersBlacklist = new Set([
+    'host',
+    'cookie',
+]);
+let serverHeadersBlacklist = new Set([
+    'set-cookie',
+    'connection',
+]);
+
 /*
 get handler handles standard GET reqs as well as streams
 */
 function get (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
 
+    // forward client headers to server
+    var headers = {};
+    for (var header in req.headers) {
+        if (!clientHeadersBlacklist.has(header.toLowerCase())) {
+            headers[header] = req.headers[header];
+        }
+    }
+
+    var options = {
+        url: req.params[0],
+        headers: headers,
+    };
+
     var data = 0; // This variable contains the size of the data (for limiting file size)
     var limit = 2e6; //TODO: change this to something different depending on tier. It's fine for now.
     request
-        .get(req.params[0]) // GET the document that the user specified
-        .on('response', function (response) {
-            res.statusCode = response.statusCode;
+        .get(options) // GET the document that the user specified
+        .on('response', function (page) {
+            res.statusCode = page.statusCode;
+
+            // include only desired headers
+            for (var header in page.headers) {
+                if (!serverHeadersBlacklist.has(header)) {
+                    res.header(header, page.headers[header]);
+                }
+            }
+            // must flush here -- otherwise pipe() will include the headers anyway!
+            res.flushHeaders();
         })
         .on('data', function (chunk) {
             data += chunk.length;
