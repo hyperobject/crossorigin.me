cdac99ecd0cfd76e11068243f4bc750b1d6b6c79
Technoboy10 <technoboy10@fake.email>
2016-11-24 15:04:05 -0700

    use es6 more, re-enable test

--- tests/integration/limits.js
+++ tests/integration/limits.js
@@ -3,14 +3,10 @@ const supertest = require('supertest');
 const server = supertest(require('../../modules/server.js'));
 
 describe('Large file request', function () {
-    // TODO find a large file on a host with CORS disabled
-
-    // it('should only serve 2MB of data', function (done) { // Will attempt to fetch a 4.3MB file
-    //     server
-    //     .get('/http://i.imgur.com/00swxq0.png') //eslint-disable-line
-    //     .expect(function (res) {
-    //         res.body.length < 2e6;
-    //     })
-    //     .expect(200, done);
-    // });
+    it('should only serve 2MB of data', function (done) { // Will attempt to fetch a 4.3MB file
+        server
+        .get('/https://01.keybase.pub/big.jpg')
+        .expect(res => res.body.length < 2e6)
+        .expect(200, done);
+    });
 });
