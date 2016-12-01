ee27970e62f5fb053422c679b51cd56a3630af4d
Technoboy10 <technoboy10@fake.email>
2016-12-01 14:23:15 -0700

    start put/post

--- modules/proxy.js
+++ modules/proxy.js
@@ -13,6 +13,7 @@ let serverHeadersBlacklist = new Set([
     'connection',
 ]);
 
+var sizeLimit = 2e6; //TODO: change this to something different depending on tier. It's fine for now.
 /*
 get handler handles standard GET reqs as well as streams
 */
@@ -22,7 +23,7 @@ function get (req, res, next) {
 
     var url = req.params[0];
 
-    // require CORS header
+    // require Origin header
     if (!requireHeader.some(header => req.headers[header])) {
         res.statusCode = 403;
         return res.end('Origin: header is required');
@@ -46,7 +47,6 @@ function get (req, res, next) {
     headers['X-Fowarded-For'] = (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress;
 
     var data = 0; // This variable contains the size of the data (for limiting file size)
-    var limit = 2e6; //TODO: change this to something different depending on tier. It's fine for now.
     request
         .get(url, {headers}) // GET the document that the user specified
         .on('response', function (page) {
@@ -68,7 +68,7 @@ function get (req, res, next) {
         })
         .on('data', function (chunk) {
             data += chunk.length;
-            if (data > limit){
+            if (data > sizeLimit){
                 res.abort(); // kills response and request cleanly
             }
         })
@@ -89,6 +89,60 @@ function post (req, res, next) {
 
 function put (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
+
+    var url = req.params[0];
+    var data = 0;
+
+    // require Origin header
+    if (!requireHeader.some(header => req.headers[header])) {
+        res.statusCode = 403;
+        return res.end('Origin: header is required');
+    }
+
+    // TODO redirect same origin
+    /* from cors-anywhere: boolean redirectSameOrigin - If true, requests to
+     * URLs from the same origin will not be proxied but redirected. The
+     * primary purpose for this option is to save server resources by
+     * delegating the request to the client (since same-origin requests should
+     * always succeed, even without proxying). */
+
+    // forward client headers to server
+
+    var headers = {};
+    for (var header in req.headers) {
+        if (!clientHeadersBlacklist.has(header.toLowerCase())) {
+            headers[header] = req.headers[header];
+        }
+    }
+
+    var forwardedFor = req.headers['X-Fowarded-For'];
+    headers['X-Fowarded-For'] = (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress;
+    req.pipe(
+        request
+        .put(url, {headers})
+        .on('data', function (chunk) {
+            data += chunk.length;
+            if (data > sizeLimit){
+                res.abort(); // kills response and request cleanly
+            }
+        })
+        .on('response', function (page) {
+            res.statusCode = page.statusCode;
+
+            // if the page already supports cors, redirect to the URL directly
+            if (page.headers['access-control-allow-origin'] === '*') { // TODO is this best?
+                res.redirect(url, next);
+            }
+
+            // include only desired headers
+            for (var header in page.headers) {
+                if (!serverHeadersBlacklist.has(header)) {
+                    res.header(header, page.headers[header]);
+                }
+            }
+            // must flush here -- otherwise pipe() will include the headers anyway!
+            res.flushHeaders();
+        }));
     next();
 }
 
