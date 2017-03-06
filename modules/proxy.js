1b5e003b94380ed1ae778fb8244fba4cb010a4d3
Technoboy10 <technoboy10@fake.email>
2017-03-05 20:08:25 -0700

    Fix query string issue

--- modules/proxy.js
+++ modules/proxy.js
@@ -23,7 +23,7 @@ function get (req, res, next) {
 
     res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
 
-    var url = req.params[0];
+    var url = req.url.substr(1);
 
     // disallow blocked phrases
     if (url.match(blockedPhrases)) {
@@ -57,7 +57,7 @@ function get (req, res, next) {
     var data = 0; // This variable contains the size of the data (for limiting file size)
     var limit = 2e6; //TODO: change this to something different depending on tier. It's fine for now.
     request
-        .get(url, {headers, qs:req.query}) // GET the document that the user specified
+        .get(url, {headers}) // GET the document that the user specified
         .on('response', function (page) {
             res.statusCode = page.statusCode;
 
