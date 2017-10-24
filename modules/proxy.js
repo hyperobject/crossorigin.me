f8a313a3b8cbad178728dc33341c5cb54d647495
tjvr <tim@tjvr.org>
2017-10-24 09:40:03 +0100

    Refactor GET and POST into one method

--- modules/proxy.js
+++ modules/proxy.js
@@ -20,11 +20,19 @@ var sizeLimit = 2e6; //TODO: change this to something different depending on tie
 /*
 get handler handles standard GET reqs as well as streams
 */
-function get (req, res, next) {
+const proxy = method => (req, res, next) => {
 
     res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
 
-    var url = req.url.substr(1);
+    let url;
+    switch (method) {
+    case 'GET':
+        url = req.url.substr(1);
+        break;
+    case 'POST':
+        url = req.params[0];
+        break;
+    }
 
     // disallow blocked phrases
     if (url.match(blockedPhrases)) {
@@ -56,8 +64,7 @@ function get (req, res, next) {
     headers['X-Fowarded-For'] = (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress;
 
     var data = 0; // This variable contains the size of the data (for limiting file size)
-    request
-        .get(url, {headers}) // GET the document that the user specified
+    request(url, {method, headers}) // request the document that the user specified
         .on('response', function (page) {
             // Check content length - if it's larger than the size limit, end the request with a 413 error.
             if (Number(page.headers['content-length']) > sizeLimit) {
@@ -91,66 +98,7 @@ function get (req, res, next) {
         })
         .pipe(res); // Stream requested url to response
     next();
-}
-
-function post (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
-
-    var url = req.params[0];
-    var data = 0;
-
-    // require Origin header
-    if (!requireHeader.some(header => req.headers[header])) {
-        res.statusCode = 403;
-        return res.end('Origin: header is required');
-    }
-
-    // TODO redirect same origin
-    /* from cors-anywhere: boolean redirectSameOrigin - If true, requests to
-     * URLs from the same origin will not be proxied but redirected. The
-     * primary purpose for this option is to save server resources by
-     * delegating the request to the client (since same-origin requests should
-     * always succeed, even without proxying). */
-
-    // forward client headers to server
-
-    var headers = {};
-    for (var header in req.headers) {
-        if (!clientHeadersBlacklist.has(header.toLowerCase())) {
-            headers[header] = req.headers[header];
-        }
-    }
-
-    var forwardedFor = req.headers['X-Fowarded-For'];
-    headers['X-Fowarded-For'] = (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress;
-    req.pipe(
-        request
-        .post(url, {headers})
-        .on('data', function (chunk) {
-            data += chunk.length;
-            if (data > sizeLimit){
-                res.abort(); // kills response and request cleanly
-            }
-        })
-        .on('response', function (page) {
-            res.statusCode = page.statusCode;
-
-            // if the page already supports cors, redirect to the URL directly
-            if (page.headers['access-control-allow-origin'] === '*') { // TODO is this best?
-                res.redirect(url, next);
-            }
-
-            // include only desired headers
-            for (var header in page.headers) {
-                if (!serverHeadersBlacklist.has(header)) {
-                    res.header(header, page.headers[header]);
-                }
-            }
-            // must flush here -- otherwise pipe() will include the headers anyway!
-            res.flushHeaders();
-        }));
-    next();
-}
+};
 
 /*
 opts handler allows us to use our own CORS preflight settings
@@ -163,5 +111,8 @@ function opts (req, res, next) { // Couple of lines taken from http://stackoverf
     res.send(200);
     next();
 }
+
+const get = proxy('GET');
+const post = proxy('POST');
 
 module.exports = {get, post, opts};
